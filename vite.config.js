b942ae51f4941519094555782c4c6414e032bced
Diego Salcedo Romero <diegosalcedoromero835@gmail.com>
2025-10-23 10:07:27 -0500

    Ajuste en el vite.config.js para que se compilen los archivos de js, css y React.js correctamente.

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,31 @@ import { defineConfig } from 'vite'
 import laravel from 'laravel-vite-plugin'
 import react from '@vitejs/plugin-react'
 import path from 'path'
+import fs from 'fs'
+
+// Función para obtener todos los archivos con ciertas extensiones
+function getAllFiles(dir, exts = ['.js'], files = []) {
+    fs.readdirSync(dir).forEach(file => {
+        const filePath = path.join(dir, file)
+        if (fs.statSync(filePath).isDirectory()) {
+            getAllFiles(filePath, exts, files)
+        } else if (exts.some(ext => file.endsWith(ext))) {
+            files.push(filePath)
+        }
+    })
+    return files
+}
+
+const jsFiles = getAllFiles('resources/js', ['.js', '.jsx', '.ts', '.tsx'])
+const cssFiles = getAllFiles('resources/css', ['.css'])
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: [
+                ...jsFiles,
+                ...cssFiles,
+            ],
             refresh: true,
         }),
         react(),
